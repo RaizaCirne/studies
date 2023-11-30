6f0ad7071e30c944e15408bdbab1d85a48aab160
Raiza Cirne <raizacirne@gmail.com>
2023-11-30 19:16:47 -0300

    Feat: add exercicio condicionais

--- JavaScript/projetos/javascript/assets/js/script.js
+++ JavaScript/projetos/javascript/assets/js/script.js
@@ -1,12 +1,28 @@
-let idade = 90; 
+// Substitua o @ pela condicional correta para que 
+// o resultado seja TRUE 
+let x = 10; 
+let y = 5; 
+console.log(x > y); 
 
-if(idade >= 18) {
-  console.log("Você é maior de idade");
+// Substitua o @ pela condicional correta para que 
+// o resultado seja FALSE 
+let w = "10"; 
+let z = 10; 
+console.log(w === z); 
+
+// Substitua o @ pela condicional correta para que 
+// o resultado seja TRUE
+let a = 10; 
+let b = 5; 
+console.log(x > y && a > b); 
+
+// Crie uma condicional para verificar se o preço 
+// da carne está barato ou caro. PS: Até 45 está barato. 
+
+let preco = 40.3; 
+
+if (preco <= 45) {
+  console.log("A carne está barata")
 } else {
-  console.log("Você é MENOR de idade");
+  console.log("A carne está cara")
 }
-
-
-
-
-
