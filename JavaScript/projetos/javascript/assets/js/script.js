2847b92a651aad0c34c43df525ce51872a955962
Raiza Cirne <raizacirne@gmail.com>
2023-11-30 19:23:26 -0300

    Feat: add condicional ternário

--- JavaScript/projetos/javascript/assets/js/script.js
+++ JavaScript/projetos/javascript/assets/js/script.js
@@ -1,28 +1,6 @@
-// Substitua o @ pela condicional correta para que 
-// o resultado seja TRUE 
-let x = 10; 
-let y = 5; 
-console.log(x > y); 
+let isMember = false; 
 
-// Substitua o @ pela condicional correta para que 
-// o resultado seja FALSE 
-let w = "10"; 
-let z = 10; 
-console.log(w === z); 
+let shipping = isMember ? 2 : 10; 
 
-// Substitua o @ pela condicional correta para que 
-// o resultado seja TRUE
-let a = 10; 
-let b = 5; 
-console.log(x != y); 
-
-// Crie uma condicional para verificar se o preço 
-// da carne está barato ou caro. PS: Até 45 está barato. 
-
-let preco = 40.3; 
-
-if (preco <= 45) {
-  console.log("A carne está barata")
-} else {
-  console.log("A carne está cara")
-}
+console.log(isMember ? "Você é membro" : "Você não é membro");
+console.log("Frete: " + shipping)
