8a71b8f1fa6b401440af418de3cb95767c775a3e
Raiza Cirne <raizacirne@gmail.com>
2023-11-30 19:39:44 -0300

    Feat: add switch

--- JavaScript/projetos/javascript/assets/js/script.js
+++ JavaScript/projetos/javascript/assets/js/script.js
@@ -1,4 +1,23 @@
-let age = 14; 
+// Switch - condicional em que temos múltiplos resultados
 
-let isAdult = ((age >= 18 && age < 60) ? true : false);
-console.log(isAdult);
+let profession = "atleta"; 
+
+console.log(`Profissão: ${profession}`);
+
+switch(profession) {
+  case 'fiscal': 
+    console.log("Sua camisa será VERDE")
+
+  break;
+  case 'bombeiro':
+    console.log("Sua camisa será VEMELHA")
+
+  break; 
+  case 'policial': 
+  console.log("Sua camisa será AZUL")
+
+  break; 
+  default: 
+  console.log("Sua camisa será PRETA");
+  break;
+}
