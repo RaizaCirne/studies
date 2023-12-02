346df4a474ad0babcedfeea1c4eb5efbae880a21
Raiza Cirne <raizacirne@gmail.com>
2023-12-02 10:11:55 -0300

    Feat: Criar e executar - Função somar

--- JavaScript/projetos/javascript/assets/js/script.js
+++ JavaScript/projetos/javascript/assets/js/script.js
@@ -1,8 +1,8 @@
-// Criando uma função
-function gravidade() {
-  console.log("A gravidade do planeta é: "); 
-  console.log(9.8);
+// Criando função 
+function somar(n1, n2) {
+  let resultado = n1 + n2; 
+  console.log("O resultado é: " + resultado);
 }
 
-// Executando a função 
-gravidade();
+// Executando a função somar 
+somar(10,15);
