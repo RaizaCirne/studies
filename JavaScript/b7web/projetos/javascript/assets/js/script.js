a8c12945054638c09c1caccdefeecefc50f4c0b7
Raiza Cirne <raizacirne@gmail.com>
2023-12-06 07:58:48 -0300

    Doc: alterar pasta js

---
+++ JavaScript/b7web/projetos/javascript/assets/js/script.js
@@ -0,0 +1,8 @@
+// Criando função 
+function nomeCompleto (nome, sobrenome) {
+  console.log(`${nome} ${sobrenome}`);
+}
+
+// Executando a função nomeCompleto
+nomeCompleto('Raíza', 'Cirne');
+nomeCompleto('Ana', 'Cozza');
--- JavaScript/projetos/javascript/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// Criando função 
-function somar(n1, n2) {
-  let resultado = n1 + n2; 
-  console.log("O resultado é: " + resultado);
-}
-
-// Executando a função somar 
-somar(10,15);
