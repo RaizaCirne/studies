1e575dbe8ce0cadb9ddc29eed3c83c93e82cdbc8
Raiza Cirne <raizacirne@gmail.com>
2023-12-06 16:29:36 -0300

    Feat: return function

--- JavaScript/b7web/projetos/javascript/assets/js/script.js
+++ JavaScript/b7web/projetos/javascript/assets/js/script.js
@@ -1,8 +1,20 @@
-// Criando função 
+// Recebendo dados
 function nomeCompleto (nome, sobrenome) {
-  console.log(`${nome} ${sobrenome}`);
+  // Processando e Retornando dados
+  return `${nome} ${sobrenome}`;
 }
 
-// Executando a função nomeCompleto
-nomeCompleto('Raíza', 'Cirne');
-nomeCompleto('Ana', 'Cozza');
+// Executando a função nomeCompleto. 
+// O retorno da função (return) vai ser armazenado na variável -> completo
+let completo = nomeCompleto('Raíza', 'Cirne');
+console.log('Completo: ' + completo);
+
+// Dando um console direto
+// Executando a função e jogando resultado para o console
+console.log(nomeCompleto('Raíza', 'Cirne'));
+
+
+
+
+// ENTRADA -> PROCESSAMENTO -> SAÍDA 
+
