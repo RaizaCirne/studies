a300a32cc35442b78f610063ff6da36c74c82138
Raiza Cirne <raizacirne@gmail.com>
2023-12-11 15:06:46 -0300

    Feat: add exercicio com switch

--- JavaScript/b7web/projetos/javascript/assets/js/exercicio02.js
+++ JavaScript/b7web/projetos/javascript/assets/js/exercicio02.js
@@ -13,22 +13,49 @@ let preco = calcularImovel(metragem, quartos);
 console.log(`A casa custa R$ ${preco}`);
 */ 
 
-let metragem = 123; 
-let quartos = 1; 
-let preco = calcularImovel(metragem, quartos); 
-console.log(`A casa custa R$ ${preco}`);
+// let metragem = 123; 
+// let quartos = 1; 
+// let preco = calcularImovel(metragem, quartos); 
+// console.log(`A casa custa R$ ${preco}`);
+
+// function calcularImovel(metragem, quartos) {
+//   let m2 = 3000; 
+
+//   if (quartos == 1) {
+//     let preco = metragem * m2; 
+//     return preco;
+//   } else if (quartos == 2) {
+//     let preco = metragem * (m2 * 1.2);
+//     return preco; 
+//   } else {
+//     let preco = metragem * (m2 * 1.5);
+//     return preco; 
+//   }
+// }
+
+
+// Usando switch 
 
 function calcularImovel(metragem, quartos) {
   let m2 = 3000; 
+  let preco = 0; 
 
-  if (quartos == 1) {
-    let preco = metragem * m2; 
-    return preco;
-  } else if (quartos == 2) {
-    let preco = metragem * (m2 * 1.2);
-    return preco; 
-  } else {
-    let preco = metragem * (m2 * 1.5);
-    return preco; 
+  switch(quartos) {
+    case 1: 
+      preco = metragem * m2; 
+      break; 
+    case 2: 
+      preco = metragem * (m2 * 1.2); 
+      break
+    case 3:
+      preco = metragem * (m2 * 1.5); 
+      break; 
   }
+
+  return preco; 
 }
+
+let metragem = 123; 
+let quartos = 2; 
+let preco = calcularImovel(metragem, quartos); 
+console.log(`A casa custa R$ ${preco}`);
