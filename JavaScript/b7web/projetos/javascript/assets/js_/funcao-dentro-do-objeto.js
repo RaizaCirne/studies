734a1a4fff4f62aa0e9eddc363b695b4291b2e9e
Raiza Cirne <raizacirne@gmail.com>
2023-12-20 09:53:22 -0300

    Feat: add funcao dentro de objeto

---
+++ JavaScript/b7web/projetos/javascript/assets/js_/funcao-dentro-do-objeto.js
@@ -0,0 +1,31 @@
+/*
+
+let pessoa = {
+  nome: 'Bonieky',
+  sobrenome: 'Lacerda',
+  idade: 90
+}
+
+// Gerando nome completo 
+console.log(pessoa.nome + ' ' + pessoa.sobrenome); 
+
+*/
+
+// Deixando o próprio objeto a cargo de gear o nome completo 
+
+let pessoa = {
+  nome: 'Bonieky',
+  sobrenome: 'Lacerda',
+  idade: 90,
+  nomeCompleto: function() {
+    return `${this.nome} ${this.sobrenome} `;
+   }
+}
+
+console.log(pessoa.nomeCompleto());
+
+// this -> se refere ao próprio objeto/ ao próprio item / acessa o próprio objeto  
+
+// A função nomeCompleto está retornando o nome concatenado com espaçinho e sobrenome. 
+
+// arrow function dentro de objeto não tem acesso ao this
