ae40d10e2659f25c537deb3d6dbf5cd0e3c93115
Raiza Cirne <raizacirne@gmail.com>
2024-01-04 17:13:20 -0300

    Feat: add desafio 03

--- JavaScript/365_js/assets/js/basico03.js
+++ JavaScript/365_js/assets/js/basico03.js
@@ -1,21 +1,10 @@
-// Escreva uma função que recebe 6 parâmetros (a,b,c,d,e,f);
-// Faça a soma de a com b.
-// O resultado da soma, subtraia o valor de c. 
-// O resultado da subtração, multiplique por d e faça a divisão por e. 
-// E para finalizar, faça o resultado elevado à potência de f. 
-
-function myFunction (a,b,c,d,e,f) {
-  // const soma = a + b; 
-  // const subtracao = soma - c; 
-  // const mutiplicacaoDivisao = subtracao * d / e;
-  // const resultado = mutiplicacaoDivisao ** f; 
-  // return resultado 
-  return (((a + b)) - c * d / e) ** f; 
+function myFunction(a, b, c, d, e, f) {
+  const calculaSomaSubtracao = ((a + b) - c); 
+  const calculaMultiplicacaoDivisao = calculaSomaSubtracao * d/ e;  
+  return resultado = calculaMultiplicacaoDivisao ** f;
 }
-
 console.log(myFunction(6,5,4,3,2,1)); // 10.5
 console.log(myFunction(6,2,1,4,2,3)); // 2744
 console.log(myFunction(2,3,6,4,2,3)); // - 8 
 
-// Add exercício 01 Janeiro
 
