8ec349e5e2beb743e76dd6ed45c6ee2623cbe3bf
Raiza Cirne <raizacirne@gmail.com>
2024-01-09 18:03:37 -0300

    Feat: add desafio 05

---
+++ JavaScript/365_js/assets/js/basico05.js
@@ -0,0 +1,9 @@
+// Complete a função 
+function myFunction(fullname, flag="") {
+  return
+}
+
+console.log(myFunction("John Williams Smith", "firstname")); // John 
+console.log(myFunction("John Williams Smith", "lastname")); // Williams Smith
+console.log(myFunction("John Williams Smith")); // John Williams Smith
+console.log(myFunction("John", "lastname")); // John 
