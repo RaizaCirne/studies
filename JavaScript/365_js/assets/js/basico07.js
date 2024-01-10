09845d2367fd93e985f96fbb6360cda0264ee968
Raiza Cirne <raizacirne@gmail.com>
2024-01-10 16:14:34 -0300

    Fix: corrigir identação

--- JavaScript/365_js/assets/js/basico07.js
+++ JavaScript/365_js/assets/js/basico07.js
@@ -6,10 +6,10 @@ console.log(
   myFunction("a", "Quantas vezes o valor de 'a' ocorre aqui?")
   ); // 5 
 
-  console.log(
-    myFunction("as", "Quantas vezes o valor de 'a' ocorre aqui?")
-    ); // 1
+console.log(
+  myFunction("as", "Quantas vezes o valor de 'a' ocorre aqui?")
+  ); // 1
 
-    console.log(
-      myFunction("q", "Quantas vezes o valor de 'a' ocorre aqui?")
-      ); // 2
+console.log(
+  myFunction("q", "Quantas vezes o valor de 'a' ocorre aqui?")
+  ); // 2
