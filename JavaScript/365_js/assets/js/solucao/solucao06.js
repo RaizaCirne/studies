f11cea413febf1d82117fcf65b805e56090158eb
Raiza Cirne <raizacirne@gmail.com>
2024-01-10 16:04:09 -0300

    Feat: add desafio 06

---
+++ JavaScript/365_js/assets/js/solucao/solucao06.js
@@ -0,0 +1,8 @@
+function myFunction(num) {
+  return num % 2 === 0;
+}
+
+console.log(myFunction(8)); // true
+console.log(myFunction(-10)); // true
+console.log(myFunction(7)); // false
+console.log(myFunction(-41)); // false
