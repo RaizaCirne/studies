4ba6580792dcf01c79d1bfb5bb5cd679ec8416ff
Raiza Cirne <raizacirne@gmail.com>
2024-01-12 19:36:08 -0300

    add desafio em andamento

---
+++ JavaScript/365_js/assets/js/basico08.js
@@ -0,0 +1,12 @@
+function myFunction(a) {
+  while (a <= 3) {
+    return a.splice();
+  }
+}
+
+console.log(1, 2, 3, 4, 5); // [4, 5]
+console.log(5, 4, 3, 2, 1, 0); // [2, 1, 0]
+console.log(18, 20, 30); // [ ]
+console.log(99, 100); // [ ]
+
+
