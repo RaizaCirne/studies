825000a71cf953f71c0725fb4c3b447180ffec3c
Raiza Cirne <raizacirne@gmail.com>
2024-01-20 13:18:17 -0300

    Feat: add solucao 12

---
+++ JavaScript/desafio_js/assets/js/solucao/solucao12.js
@@ -0,0 +1,9 @@
+function myFunction(a, b) {
+  return a.reduce(function(prev, cur) {
+    return cur > b ? prev + cur : prev 
+  }, 0)
+}
+
+console.log(myFunction([1, 2, 3, 4, 5, 6, 7, 8], 3)); // 30 
+console.log(myFunction([-14, -12, -5, 4, -4, -5])); // 0
+console.log(myFunction([33, 45, 88, 105, 111], 99)); // 216 
