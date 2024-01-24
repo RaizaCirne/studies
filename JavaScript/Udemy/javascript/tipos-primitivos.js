e4dd633598e23dda6b8d28f6d6652fbe615c806f
Raiza Cirne <raizacirne@gmail.com>
2024-01-24 17:41:43 -0300

    Feat: add objeto js

---
+++ JavaScript/Udemy/javascript/tipos-primitivos.js
@@ -0,0 +1,8 @@
+let itemName = 'Pen'; // string
+let itemPrice = 3; // number 
+let itemAvailable = true; // boolean 
+let itemColor // undefined 
+itemName = null // null 
+
+// verificando tipo da variável 
+console.log(typeof itemAvailable);
