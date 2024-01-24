abf3f0f9a1560cbf768e621c6ab49dc7717666c4
Raiza Cirne <raizacirne@gmail.com>
2024-01-24 20:40:50 -0300

    Feat: add funcoes

---
+++ JavaScript/Udemy/javascript/js/arrays.js
@@ -0,0 +1,9 @@
+// lista de itens 
+let friends = ['Marcos', 'Silvia', 'Elisa'];
+
+// substituindo Elisa por Junior
+friends[2] = 'Junior'; 
+console.log(friends);
+
+// mostrando no console apenas o nome da Silvia 
+console.log(friends[1]);
