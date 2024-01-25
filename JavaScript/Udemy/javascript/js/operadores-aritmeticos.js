311b13eb4f659b46dcab7e94341e466b797c8e7a
Raiza Cirne <raizacirne@gmail.com>
2024-01-25 10:09:11 -0300

    Feat: add operadores aritmeticos

---
+++ JavaScript/Udemy/javascript/js/operadores-aritmeticos.js
@@ -0,0 +1,9 @@
+let num1 = 3; 
+let num2 = 2; 
+
+console.log(num1 + num2); // soma
+console.log(num1 - num2); // subtração
+console.log(num1 * num2); // multiplicação 
+console.log(num1 / num2); // divisão
+console.log(num1 % num2); // resto da divisão
+console.log(num1 ** num2); // exponencial 
