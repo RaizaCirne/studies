dfc8ec871d1303bc75ba8266ce1e99a0bf447212
Raiza Cirne <raizacirne@gmail.com>
2024-01-24 21:14:33 -0300

    Feat: add desafio porcentagem

---
+++ JavaScript/challenges_js/02/assets/js/03.js
@@ -0,0 +1,8 @@
+// 2) Porcemtagem 
+// Uma função que recebe dois números (a e b). Deve retornar quantos % de b é de a. 
+
+function percentage (nominator, denominator ) {
+  return nominator / denominator; 
+}
+
+console.log(percentage(100, 10));
