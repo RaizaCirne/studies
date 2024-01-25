96150debe9d9e2325e169da573fb5bf23846e98f
Raiza Cirne <raizacirne@gmail.com>
2024-01-25 10:59:30 -0300

    Feat: add operadores de comparacao

---
+++ JavaScript/Udemy/javascript/operadores-comparacao.js
@@ -0,0 +1,19 @@
+
+let num1 = 3; 
+console.log(num1 > 0); // true
+console.log(num1 < 0); // false
+console.log(num1 >= 3); // true
+console.log(num1 <= 3); // true
+
+// === verifica se é estritamente igual/ identico 
+console.log(num1 === 3); // true
+
+// !== verifica se é diferente/ não é identico 
+console.log(num1 !== 3); // false
+
+
+
+
+
+
+
