2343573c0b7e028e3ed34ee47fb23ae192327edb
Raiza Cirne <raizacirne@gmail.com>
2024-01-29 14:07:08 -0300

    Feat: add for loop treino

--- JavaScript/Udemy/javascript/testes_js/foor-lop.js
+++ JavaScript/Udemy/javascript/testes_js/foor-lop.js
@@ -1,4 +1,4 @@
-// Exibir/CONTAR  na tela a numeração de 1 a 20
-for (i = 1; i <= 20; i++) {
+// Exibir/CONTAR  na tela a numeração de 10 a 20
+for (i = 10; i <= 20; i++) {
   console.log(`Número ${i}`);
 }
