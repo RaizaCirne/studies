73ec8c1959d62520ed5c43d7adb8ff6c91f3e415
Raiza Cirne <raizacirne@gmail.com>
2024-01-29 15:01:40 -0300

    Feat: add Do WHILE

---
+++ JavaScript/Udemy/javascript/js/while-loop.js
@@ -0,0 +1,14 @@
+// Exibir/CONTAR na tela a numeração de 1 a 10 (WHILE)
+
+/*
+for (i = 1; i <= 10; i++) {
+  console.log('Número ' + i);
+}; 
+*/ 
+
+i = 1; 
+
+while(i <= 10) {
+  console.log('Número ', i) 
+  i ++; 
+}
