eca9c23d76dcc3c2999b25d0c381ad1a6d874a85
Raiza Cirne <raizacirne@gmail.com>
2024-01-29 13:55:04 -0300

    Feat: for loop

---
+++ JavaScript/Udemy/javascript/js/for-loop.js
@@ -0,0 +1,4 @@
+// Exibir na tela a numeração de 1 a 10
+for (i = 1; i <= 10; i++) {
+  console.log('Número ' + i);
+};
---
+++ JavaScript/Udemy/javascript/testes_js/foor-lop.js
@@ -0,0 +1,4 @@
+// Exibir na tela a numeração de 1 a 20
+for (i = 1; i <= 20; i++) {
+  console.log(`Número ${i}`);
+}
