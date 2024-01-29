3d4869bc88a43be45ebe24a472d3232b18d4650b
Raiza Cirne <raizacirne@gmail.com>
2024-01-29 20:09:52 -0300

    Feat: add function com prompt

---
+++ JavaScript/workover/simplefunction/hellofunction.js
@@ -0,0 +1,9 @@
+// Funções permitem agrupar uma série de instruções para realizar uma tarefa específica 
+
+// Se diferentes partes de um script repetirem a mesma tarefa, você pode reutilizar a função ao invés de repetir o mesmo conjunto de instruções 
+
+
+function hello() {
+  let nome = prompt('Qual é o seu nome?');
+  alert('Hello ' + nome);
+}
