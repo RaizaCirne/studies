85aa88f64b04fe577423c3380ab8b22c291389e3
Raiza Cirne <raizacirne@gmail.com>
2024-02-01 18:22:53 -0300

    feat: add classe

--- JavaScript/b7web/projetos/javascript/assets/js/script07.js
+++ JavaScript/b7web/projetos/javascript/assets/js/script07.js
@@ -1,11 +1,6 @@
 function clicou() {
   const button = document.querySelector('button');
 
-  if(button.classList.contains('azul')) {
-    button.classList.remove('azul');
-    button.classList.add('verde');
-  } else {
-    button.classList.add('azul');
-    button.classList.remove('verde');
-  }
+  // button.classList.toggle('azul');
+  button.classList.replace('azul', 'verde');
 }
