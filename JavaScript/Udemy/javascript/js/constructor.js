4845f66dac869ac5980fa14a07fcc48109af8f89
Raiza Cirne <raizacirne@gmail.com>
2024-02-06 21:58:25 -0300

    Feat: constructor

---
+++ JavaScript/Udemy/javascript/js/constructor.js
@@ -0,0 +1,15 @@
+function CreateBook(title, author, pages) {
+  // const book = {
+    this.bookTitle = title;
+    this.bookAuthor = author;
+    this.bookPages = pages;
+  // }
+  // return book;
+}
+
+const book1 = new CreateBook('Atomic Habits', 'James Clear', 306);
+
+console.log(book1);
+
+
+
