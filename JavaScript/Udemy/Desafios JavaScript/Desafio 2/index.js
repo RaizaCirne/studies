f0f8a14493acccbd75d444ad8a69021bd70811dc
Raiza Cirne <raizacirne@gmail.com>
2024-02-20 07:15:55 -0300

    Feat: add prompt number

--- JavaScript/Udemy/Desafios JavaScript/Desafio 2/index.js
+++ JavaScript/Udemy/Desafios JavaScript/Desafio 2/index.js
@@ -1,8 +1,1 @@
-var valor = Number(prompt('Qual valor você pode pagar?'));
-const valorProduto = 20;
-
-if (valor < 20) {
-  document.write(`Você só tem ${valor}. O produto custa $${valorProduto}`);
-} else {
-  document.write(`Você tem $${valor}, escolha o seu produto! `);
-}
+let productValue = Number(prompt('Enter the product value: '));
