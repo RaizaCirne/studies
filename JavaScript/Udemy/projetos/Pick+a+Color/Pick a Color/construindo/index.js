998ddf6ac14cde9b03ca5cfcbc0a660d2177677d
Raiza Cirne <raizacirne@gmail.com>
2024-04-03 16:42:13 -0300

    Feat: add evento de click no botão

--- JavaScript/Udemy/projetos/Pick+a+Color/Pick a Color/construindo/index.js
+++ JavaScript/Udemy/projetos/Pick+a+Color/Pick a Color/construindo/index.js
@@ -1,3 +1,8 @@
 // Adicionando variavéis
 const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
 const btn = document.getElementById('btn')
+
+// Adicionando ações
+btn.addEventListener('click', function() {
+  document.body.style.background = colors[4];
+})
