f83ff0cf1d7bf601ee469f1da8bb7d627d8d02e6
Raiza Cirne <raizacirne@gmail.com>
2024-04-09 15:43:46 -0300

    Feat: add propriedades React

--- Reactjs/aulas/01-fundamentos-reactjs/src/App.jsx
+++ Reactjs/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -3,11 +3,15 @@ import { Post } from './Post';
 export function App() {
   return (
       <div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post 
+          author="Diego Fernandes" 
+          content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur esse atque ipsam et similique quis aspernatur tenetur aperiam eius tempore harum perferendis molestias in impedit ratione, corporis ab ipsum excepturi!" 
+        />
+        <Post 
+        author="Gabriel Buzzi" 
+        content="Um novo post muito legal" 
+        />
       </div>
   )
 }
+
--- Reactjs/aulas/01-fundamentos-reactjs/src/Post.jsx
+++ Reactjs/aulas/01-fundamentos-reactjs/src/Post.jsx
@@ -1,4 +1,10 @@
-export function Post() {
-  return <p>Post</p>
+export function Post(props) {
+
+  return (
+  <div>
+    <strong>{props.author}</strong>
+    <p>{props.content}</p>
+  </div>
+)
 }
 
