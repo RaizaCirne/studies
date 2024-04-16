3752656397cb98c8e38ac9f7dbfb4eeff25d0624
Raiza Cirne <raizacirne@gmail.com>
2024-04-16 15:56:26 -0300

    Feat: Add counter

--- JavaScript/Udemy/projetos/Guess+the+Number/Guess the Number/construindo/index.js
+++ JavaScript/Udemy/projetos/Guess+the+Number/Guess the Number/construindo/index.js
@@ -1,6 +1,7 @@
 
 let computerNumber
 let userNumbers = []
+let attempts = 0
 
 function init() {
    computerNumber = Math.floor(Math.random() * 100 + 1)
@@ -15,10 +16,18 @@ function compareNumbers() {
    if(userNumber > computerNumber) {
       document.getElementById('textOutput').innerHTML = 'Your number is too high'
       document.getElementById('inputBox').value = ''
+      attempts++ 
+      document.getElementById('attempts').innerHTML = attempts
+
    } else if (userNumber < computerNumber) {
       document.getElementById('textOutput').innerHTML = 'Your number is too low'
       document.getElementById('inputBox').value = ''
+      attempts++ 
+      document.getElementById('attempts').innerHTML = attempts
+      
    } else {
        document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
+       attempts++ 
+       document.getElementById('attempts').innerHTML = attempts
    }
 }
