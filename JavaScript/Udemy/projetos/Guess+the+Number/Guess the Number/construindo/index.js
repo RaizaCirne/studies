15de11ee5364f795a3c563aafc3da48e8d4e5e44
Raiza Cirne <raizacirne@gmail.com>
2024-04-18 22:37:21 -0300

    Feat: add if else

--- JavaScript/Udemy/projetos/Guess+the+Number/Guess the Number/construindo/index.js
+++ JavaScript/Udemy/projetos/Guess+the+Number/Guess the Number/construindo/index.js
@@ -2,6 +2,7 @@
 let computerNumber
 let userNumbers = []
 let attempts = 0
+let MaxGuesses = 10
 
 function init() {
    computerNumber = Math.floor(Math.random() * 100 + 1)
@@ -13,21 +14,26 @@ function compareNumbers() {
    userNumbers.push(' ' + userNumber)
    document.getElementById('guesses').innerHTML = userNumbers
 
-   if(userNumber > computerNumber) {
-      document.getElementById('textOutput').innerHTML = 'Your number is too high'
-      document.getElementById('inputBox').value = ''
-      attempts++ 
-      document.getElementById('attempts').innerHTML = attempts
-
-   } else if (userNumber < computerNumber) {
-      document.getElementById('textOutput').innerHTML = 'Your number is too low'
-      document.getElementById('inputBox').value = ''
-      attempts++ 
-      document.getElementById('attempts').innerHTML = attempts
-      
-   } else {
-       document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
-       attempts++ 
-       document.getElementById('attempts').innerHTML = attempts
+   if(attempts < MaxGuesses) {
+      if(userNumber > computerNumber) {
+         document.getElementById('textOutput').innerHTML = 'Your number is too high'
+         document.getElementById('inputBox').value = ''
+         attempts++ 
+         document.getElementById('attempts').innerHTML = attempts
+   
+      } else if (userNumber < computerNumber) {
+         document.getElementById('textOutput').innerHTML = 'Your number is too low'
+         document.getElementById('inputBox').value = ''
+         attempts++ 
+         document.getElementById('attempts').innerHTML = attempts
+         
+      } else {
+          document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
+          attempts++ 
+          document.getElementById('attempts').innerHTML = attempts
+      }
+   } 
+   else {
+      document.getElementById('textOutput').innerHTML = 'You Lose! The computer number was ' + computerNumber
    }
 }
