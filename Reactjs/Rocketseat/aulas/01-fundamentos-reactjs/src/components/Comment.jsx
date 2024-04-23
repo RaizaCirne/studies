366e869f032639d84ce3955ca0a4f4713490b137
Raiza Cirne <raizacirne@gmail.com>
2024-04-23 18:00:11 -0300

    Fet: Add mobile

--- Reactjs/Rocketseat/aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ Reactjs/Rocketseat/aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -5,13 +5,13 @@ import { Avatar } from './Avatar';
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/pedromarins.png" />
+      <Avatar hasBorder={false} src="https://github.com/RaizaCirne.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Pedro Marins</strong>
+              <strong>Raíza Cirne</strong>
               <time title='11 de Maio às 08:13' dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
             </div>
              
