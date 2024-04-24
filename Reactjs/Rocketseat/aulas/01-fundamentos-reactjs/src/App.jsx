f680e5b0cd737cd0b69be35826148c2b4aa7cc38
Raiza Cirne <raizacirne@gmail.com>
2024-04-24 11:44:11 -0300

    Feat: Add array of objects

--- Reactjs/Rocketseat/aulas/01-fundamentos-reactjs/src/App.jsx
+++ Reactjs/Rocketseat/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -5,6 +5,47 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 
 import './global.css';
+import { Paragraph } from '@phosphor-icons/react';
+// author: { avatar_url: “”, name: “”, role: “”}
+// publishedAt: Date
+// content: String
+
+// array com todos os posts que a aplicação vai ter
+// Como o post tem várias informações dentro dele, cada posição dentro do array vai ser um objeto.
+// Um array de objetos: um array com vários posts 
+
+
+const post = [
+  {
+    id: 1, 
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes', 
+      role: 'CTO @Rocketseat'
+    },
+    // cada linha do post como sendo uma posição no array
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋' },
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-04-23 20:00:00'),
+  },
+  {
+    id: 2, 
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito', 
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋' },
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-04-22 20:00:00'),
+  },
+]; 
 
 export function App() {
   return (
@@ -14,17 +55,17 @@ export function App() {
         <div className={styles.wrapper}>
           <Sidebar />
           <main>
-              <Post 
-                author="Diego Fernandes" 
-                content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur esse atque ipsam et similique quis aspernatur tenetur aperiam eius tempore harum perferendis molestias in impedit ratione, corporis ab ipsum excepturi!" 
-            />
-            <Post 
-              author="Gabriel Buzzi" 
-              content="Um novo post muito legal" 
-            />
+              {post.map(post => {
+                return (
+                  < Post 
+                  author = {post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                  />
+              )
+              })}
           </main>
         </div>
       </div>
   )
 }
-
