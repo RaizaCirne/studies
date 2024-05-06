43f27382fda39c7b7ced4e0703d98c9ebcfe734b
Raiza Cirne <raizacirne@gmail.com>
2024-05-06 07:01:48 -0300

    Style: Rearrange styles

--- Mentoria Vip/desafios/netflix/assets/js/script.js
+++ Mentoria Vip/desafios/netflix/assets/js/script.js
@@ -6,6 +6,11 @@ faqs.forEach((faq) => {
       faq.classList.remove("active");
     } else {
       faq.classList.add("active");
+      faqs.forEach((otherFaq) => {
+        if (otherFaq != faq) {
+          otherFaq.classList.remove("active");
+        }
+      });
     }
   });
 });
