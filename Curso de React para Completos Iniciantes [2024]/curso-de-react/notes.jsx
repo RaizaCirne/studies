14473ccb4cefc3c34fb29d88bc1b44be1bb0145e
Raiza Cirne <raizacirne@gmail.com>
2024-11-16 16:03:11 -0300

    Feat: Add notes

---
+++ Curso de React para Completos Iniciantes [2024]/curso-de-react/notes.jsx
@@ -0,0 +1,43 @@
+// # Requisitos para o curso
+
+//   - HTML & CSS 
+//   - JavaScript (ES6+)
+//     - Estrutura de Dados, funções, objetos  
+//     - Destruturing, arrow functions 
+//     - Promises & Async/Await 
+
+// ## O que é o React?
+// ### O React é uma biblioteca usada para criar interfaces de usuário interativas.  
+
+// ## Por que aprender React?
+// ### - Desenvolvida pelo Facebook (Meta)
+// ### - Aprenda uma vez, use em qualquer lugar
+// ###    - iOS & Android (React Native) & Desktop (Electron)
+
+// ### - É a tecnologia mais demandada pelo mercado autalmente (e isso já há vários anos)
+// ###    - É praticamente o padrão da indústria quando o assunto é criação de aplicações web 
+// ###    - Sai muito na frente de seus concorrentes (Vue e Angular)
+
+// ### - É utilizado por empresas multibilionárias: Facebook, Instagram, Netflix, Airbnb, LinkedIn, Spotify. 
+
+
+// ## Componentes
+// ### - No React, dividimos a aplicação em pequenos componentes 
+// ### - Vários componentes, quando combinados, formam grandes estruturas
+// ### - Pense neles como se fossem peças de LEGO 
+// ### - Usamos JSX para escrevê-los 
+// *JSX -> html misturado com js 
+
+// node -> ferramenta que permite que você execute código js fora do navegador e precisamos dele para criar projetos react. 
+// npm -> ao instalar o node ganhamos o npm -> é um gerenciador de biblioteca para o node -> biblioteca -> basicamente uma extensão que você consegue usar no projeto (para solucionar problemas)
+
+
+
+
+
+
+
+
+
+
+
