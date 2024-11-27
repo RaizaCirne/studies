7df77652e1c63f69ab51b524f090ae59e0a7b51b
Raiza Cirne <raizacirne@gmail.com>
2024-11-27 18:13:25 -0300

    feat: add styled-components for component styling

--- Next Academy/Mentoria VIP - Alexandre Saints/Módulo 3 - Bem vindo ao React/aula5/src/pages/home/index.js
+++ Next Academy/Mentoria VIP - Alexandre Saints/Módulo 3 - Bem vindo ao React/aula5/src/pages/home/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import home from "../../assets/images/home.jpg";
+import { HeaderModificada } from "./styles";
 
 export default function Home() {
   return (
     <>
-      <header>
+      <HeaderModificada>
         <h1>Título da Página Home</h1>
         <img src={home} alt="Desenho de uma casa" style={{ width: 700 }} />
-      </header>
+      </HeaderModificada>
       <h1>Olá, essa é a página Home</h1>
     </>
   );
---
+++ Next Academy/Mentoria VIP - Alexandre Saints/Módulo 3 - Bem vindo ao React/aula5/src/pages/home/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HeaderModificada = styled.header`
+  display: flex;
+  flex-direction: row;
+  background-color: purple;
+  justify-content: space-between;
+  align-items: center;
+`;
