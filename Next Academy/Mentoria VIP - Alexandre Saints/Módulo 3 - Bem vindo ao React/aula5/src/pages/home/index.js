f71136967261e771c5238b73c1ab4f8164a5bde0
Raiza Cirne <raizacirne@gmail.com>
2024-11-27 16:53:48 -0300

    style(header): add image to the header

--- Next Academy/Mentoria VIP - Alexandre Saints/Módulo 3 - Bem vindo ao React/aula5/src/pages/home/index.js
+++ Next Academy/Mentoria VIP - Alexandre Saints/Módulo 3 - Bem vindo ao React/aula5/src/pages/home/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import home from "../../assets/images/home.png";
+import home from "../../assets/images/home.jpg";
 
 export default function Home() {
   return (
     <>
       <header>
         <h1>Título da Página Home</h1>
-        <img src={home} style={{ width: 700 }} />
+        <img src={home} alt="Desenho de uma casa" style={{ width: 700 }} />
       </header>
       <h1>Olá, essa é a página Home</h1>
     </>
